28094affdade8ba1beac0635599c83bd85837409
Zorica <riotgames2409@gmail.com>
2024-02-08 00:35:01 +0100

    push

---
+++ src/Layouts/Sidebar/index.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import '../../Assets/Styles/Sidebar.css'; // Import your sidebar-specific styles
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  const [theme, setTheme] = useState('default');
+
+  const handleThemeChange = (selectedTheme) => {
+    setTheme(selectedTheme);
+  };
+
+  return (
+    <div className={`sidebar ${theme}`}>
+      <div className="sidebar-top">
+        <i className="logo fab fa-sketch"></i>
+        <span className="brand">The App</span>
+      </div>
+      <div className="sidebar-center">
+        <ul className="list">
+        <li className={`list-item ${theme === 'default' ? 'active' : ''}`}>
+            <i className="list-item-icon fas fa-home"></i>
+            <span className="list-item-text">Overview</span>
+          </li>
+          <li className="list-item">
+            <Link to="ToDo"><i className="list-item-icon fas fa-search"></i>
+            <span className="list-item-text">Tasks</span></Link>
+            
+          </li>
+          <li className="list-item">
+            <i className="list-item-icon fas fa-stream"></i>
+            <span className="list-item-text">Notes</span>
+          </li>
+
+        </ul>
+        
+      </div>
+
+      <div className="sidebar-bottom">
+      <button className="color-box dark" onClick={() => handleThemeChange('default')}>
+      
+      </button>
+      <button className="color-box night" onClick={() => handleThemeChange('night')}>
+       
+      </button>
+      <button className="color-box light" onClick={() => handleThemeChange('light')}>
+      
+      </button>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Sidebar;
