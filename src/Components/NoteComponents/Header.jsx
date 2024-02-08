a3117d6d24af9e3a4877260d51a5a9ad700b2824
Zorica <riotgames2409@gmail.com>
2024-02-08 02:42:43 +0100

    push

---
+++ src/Components/NoteComponents/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+      <div className="header">
+        <h1 className="notes__title">Notes</h1>
+      </div>
+    );
+  }
+  
+  export default Header;
+  
--- src/Pages/Notes/index.jsx
+++ src/Pages/Notes/index.jsx
@@ -1,7 +1,14 @@
+import "../../Assets/Styles/Note.css";
+import Notess from "../../Components/NoteComponents/Notes";
+import Header from "../../Components/NoteComponents/Header";
+
 
 const Notes = () => {
     return (
-        <div className="black">Notesnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</div>
-    )
+        <div className="main">
+            <Header />
+            <Notess />
+        </div>
+    );
 }
 export default Notes;
